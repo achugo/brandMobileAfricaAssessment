b604bc0cae69e55b6aa6c47b5a2f002bfd8260dc
achugo <cachugo@infomall.ng>
2020-05-25 11:02:58 +0100

    remove middleware

--- src/store/store.js
+++ src/store/store.js
@@ -1,18 +1,6 @@
-﻿import { createStore, compose, applyMiddleware } from "redux";
-import thunk from "redux-thunk";
+﻿import { createStore } from "redux";
 import { rootReducer } from "./rootReducer";
 
-const initialState = {};
-
-const middleware = [thunk];
-
-const store = createStore(
-  rootReducer,
-  initialState,
-  compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
-);
+const store = createStore(rootReducer);
 
 export default store;
